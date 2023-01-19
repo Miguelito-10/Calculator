72fc6f213ff863b37fb65b9da578109419cc4e9e
Miguel <maickys123@gmail.com>
2023-01-18 20:28:08 -0500

    the last change of the project.Project completion

--- js/display.js
+++ js/display.js
@@ -8,12 +8,15 @@ class Display {
         this.previousValue = '';
         this.sings = {
             plus: '+',
-            split: '%',
+            split: '/',
             multiply: 'x',
             subtract: '-', 
         }
     }
-
+    printValues() {
+        this.displayCurrentValue.textContent = this.currentValue;
+        this.displayPreviousValue.textContent = `${this.previousValue} ${this.sings[this.typeOperation] || ''}`;
+    }
     delete() {
         this.currentValue = this.currentValue.toString().slice(0,-1);
         this.printValues();
@@ -25,31 +28,23 @@ class Display {
         this.typeOperation= undefined;
         this.printValues();
     }
-
-    compute(type) {
-        this.typeOperation !== 'same' && this.calculate();
-        this.typeOperation = type;
-        this.previousValue = this.currentValue || this.previousValue;
-        this.currentValue = '';
-        this.printValues();
-    }
-
     addNumber(number) {
         if(number === '.' && this.currentValue.includes('.')) return
         this.currentValue = this.currentValue.toString() + number.toString();
         this.printValues();
     }
-
-    printValues() {
-        this.displayCurrentValue.textContent = this.currentValue;
-        this.displayPreviousValue.textContent = `${this.previousValue} ${this.sings[this.typeOperation] || ''}`;
-    }
-
     calculate() {
         const previousValue = parseFloat(this.previousValue);
         const currentValue = parseFloat(this.currentValue);
 
         if( isNaN(currentValue)  || isNaN(previousValue) ) return
         this.currentValue = this.calculator[this.typeOperation](previousValue, currentValue);
+    }
+    compute(type) {
+        this.typeOperation !== 'same' && this.calculate();
+        this.typeOperation = type;
+        this.previousValue = this.currentValue || this.previousValue;
+        this.currentValue = '';
+        this.printValues();
     }
 }
